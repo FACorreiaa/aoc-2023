ec3aac23979cb4636e5f68820ff2835687b4df97
FACorreiaa <fernandocorreia316@gmail.com>
2023-12-04 01:16:13 +0100

    finish day one aoc

--- day-one/main.go
+++ day-one/main.go
@@ -1,31 +1,99 @@
 package day_one
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 	"unicode"
 )
 
-func ExtractCalibrationValue(line string) int {
-	var firstValue, lastValue rune
+var (
+	lookup = []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+)
 
-	for _, char := range line {
-		if unicode.IsDigit(char) || unicode.IsLetter(char) {
-			firstValue = char
-			break
+func PartOne() {
+	lines := getLines(os.Args[1])
+	numbers := extractNumbers(lines)
+	fmt.Println(sum(numbers))
+}
+
+func PartTwo() {
+	lines := getLines(os.Args[1])
+	lines = replaceAlphaNumbers(lines)
+	numbers := extractNumbers(lines)
+	fmt.Println(sum(numbers))
+}
+
+func sum(nums []int) int {
+	var result int
+	for _, i := range nums {
+		result += i
+	}
+
+	return result
+}
+
+func replaceAlphaNumbers(lines []string) []string {
+	var output []string
+	for _, line := range lines {
+		output = append(output, replaceAlphaNumber(line))
+	}
+
+	return output
+}
+
+func replaceAlphaNumber(line string) string {
+	for index, word := range lookup {
+		line = strings.ReplaceAll(line, word, fmt.Sprintf("%s%d%s", word, index+1, word))
+	}
+
+	return line
+}
+
+func extractNumbers(lines []string) []int {
+	var numbers []int
+	for _, line := range lines {
+		numbers = append(numbers, extractNumber(line))
+	}
+
+	return numbers
+}
+
+func extractNumber(line string) int {
+	var digits []rune
+	for _, digit := range line {
+		if unicode.IsNumber(digit) {
+			digits = append(digits, digit)
 		}
 	}
 
-	for i := len(line) - 1; i >= 0; i-- {
-		if unicode.IsDigit(rune(line[i])) || unicode.IsLetter(rune(line[i])) {
-			lastValue = rune(line[i])
-			break
-		}
+	num, err := strconv.Atoi(string([]rune{digits[0], digits[len(digits)-1]}))
+	if err != nil {
+		panic(err)
 	}
 
-	if firstValue == 0 || lastValue == 0 {
-		return 0
+	return num
+}
+
+func getLines(filename string) []string {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
 	}
 
-	res := int(firstValue-'0')*10 + int(lastValue-'0')
-	println(res)
-	return res
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if scanner.Err() != nil {
+		panic(err)
+	}
+
+	return lines
 }
--- main.go
+++ main.go
@@ -1,33 +1,10 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"github.com/FACorreiaa/aoc-2023/day-one"
-	"os"
 )
 
 func main() {
-	sum := 0
-	file, err := os.Open("./day-one/calibration.txt")
-	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		//for part two
-		calibrationValue := day_one.ExtractCalibrationValue(line)
-		sum += calibrationValue
-	}
-
-	fmt.Println("Sum of calibration values:", sum)
-
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error reading file:", err)
-	}
+	day_one.PartOne()
+	day_one.PartTwo()
 }
